e6ebb777b7883866ebb7517f1f6096cf1bc0fdd5
David Baiden Amissah <davidbamissah@gmail.com>
2025-09-07 23:57:55 +0000

    k

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,18 +3,18 @@ from . import views
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    # FBV for books
+    # Function-Based View for listing books
     path('books/', views.list_books, name='list_books'),
 
-    # CBV for library detail
+    # Class-Based View for library detail
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
-    # Authentication URLs
+    # User authentication URLs
     path('register/', views.register_view, name='register'),
     path('login/', LoginView.as_view(template_name="relationship_app/login.html"), name='login'),
     path('logout/', LogoutView.as_view(template_name="relationship_app/logout.html"), name='logout'),
 
-    # Secured Book Management URLs
+    # Book management with permissions
     path('books/add/', views.add_book, name='add_book'),
     path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
     path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
