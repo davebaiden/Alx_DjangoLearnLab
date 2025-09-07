a496f23330ea7e271ccf4a485ab0b8d834b694a8
David Baiden Amissah <davidbamissah@gmail.com>
2025-09-07 23:27:08 +0000

    m

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView  # ✅ checker wants this
 from .models import Library
 from .models import Book
 
