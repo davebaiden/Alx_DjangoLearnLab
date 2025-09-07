d6ee46747401748a321752b2b19edcd979b75678
David Baiden Amissah <davidbamissah@gmail.com>
2025-09-07 23:25:34 +0000

    m

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library  # Library is imported
-              
+from .models import Library
+from .models import Book
+
 # Function-Based View (FBV) to list all books
 def list_books(request):
     books = Book.objects.all()
