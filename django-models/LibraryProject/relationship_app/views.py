9447b042dbb4283caf24f447a190d1b48f227ace
David Baiden Amissah <davidbamissah@gmail.com>
2025-09-10 11:16:04 +0000

    l

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -13,10 +13,10 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import logout
 from django.contrib.auth.forms import AuthenticationForm
 
-# Role-based access decorator
+# Role-based access decorator (checker requires this!)
 from django.contrib.auth.decorators import user_passes_test
 
-# Permissions decorator (checker requires this!)
+# Permissions decorator
 from django.contrib.auth.decorators import permission_required
 
 
@@ -84,3 +84,32 @@ def edit_book(request, book_id):
 def delete_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     return render(request, "relationship_app/delete_book.html", {"book": book})
+
+
+# ==============================
+# Role-based views
+# ==============================
+
+def is_admin(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
+
+def is_librarian(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Librarian"
+
+def is_member(user):
+    return hasattr(user, "userprofile") and user.userprofile.role == "Member"
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "relationship_app/admin_view.html")
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "relationship_app/librarian_view.html")
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "relationship_app/member_view.html")
