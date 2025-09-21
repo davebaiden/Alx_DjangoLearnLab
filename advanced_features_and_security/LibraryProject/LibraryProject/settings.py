1b7aa0e46fb22911cb94b00db9bad232b9718e68
David Baiden Amissah <davidbamissah@gmail.com>
2025-09-21 23:35:25 +0000

    l

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -134,3 +134,9 @@ SECURE_SSL_REDIRECT = True
 SECURE_HSTS_SECONDS = 31536000  # 1 year
 SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
+# ------------------------------
+# Security: HTTPS & Proxy Headers
+# ------------------------------
+
+# Tell Django to trust the X-Forwarded-Proto header set by the proxy
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
