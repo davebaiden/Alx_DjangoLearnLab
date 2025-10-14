b32dff702e5671f7d9911252fa8a56f2d072a4be
David Baiden Amissah <davidbamissah@gmail.com>
2025-10-14 03:54:18 +0000

    l

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -3,8 +3,16 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    # Auth (login/logout use Django built-ins)
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
+
+    # Posts CRUD
+    path('posts/', views.PostListView.as_view(), name='posts-list'),                    # /posts/
+    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),             # /posts/new/
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),        # /posts/<pk>/
+    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),   # /posts/<pk>/edit/
+    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'), # /posts/<pk>/delete/
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,13 +1,17 @@
-from django.contrib.auth import views as auth_views
-
-# Create your views here.
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.decorators import login_required
-# Extend UserCreationForm
+
+from .models import Post
+from .forms import PostForm
+
+# ---------- Authentication / Registration (kept) ----------
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
@@ -25,10 +29,53 @@ def register(request):
     else:
         form = CustomUserCreationForm()
     return render(request, 'blog/register.html', {'form': form})
+
 @login_required
 def profile(request):
     if request.method == 'POST':
         email = request.POST.get('email')
-        request.user.email = email
-        request.user.save()
+        if email is not None:
+            request.user.email = email
+            request.user.save()
     return render(request, 'blog/profile.html')
+
+# ---------- CRUD Views for Post ----------
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/posts_list.html'   # template path
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+    paginate_by = 10
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        # set the author to the logged-in user before saving
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('posts-list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
