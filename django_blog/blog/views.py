724bd08aaaa91e57ef54effab73edb86c573f3fe
David Baiden Amissah <davidbamissah@gmail.com>
2025-10-14 03:29:35 +0000

    l

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.contrib.auth import views as auth_views
 
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+# Extend UserCreationForm
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        request.user.email = email
+        request.user.save()
+    return render(request, 'blog/profile.html')
